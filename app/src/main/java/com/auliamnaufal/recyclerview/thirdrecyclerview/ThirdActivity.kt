3f34561825ce99a4ca64834099a5b563b0899936
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-08-05 10:42:23 +0700

    (temporary): add data to recyclerview on more complex json file

--- app/src/main/java/com/auliamnaufal/recyclerview/thirdrecyclerview/ThirdActivity.kt
+++ app/src/main/java/com/auliamnaufal/recyclerview/thirdrecyclerview/ThirdActivity.kt
@@ -1,10 +1,9 @@
 package com.auliamnaufal.recyclerview.thirdrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.auliamnaufal.recyclerview.R
 import com.auliamnaufal.recyclerview.databinding.ActivityThirdBinding
 import org.json.JSONArray
 import org.json.JSONException
@@ -35,36 +34,50 @@ class ThirdActivity : AppCompatActivity() {
     }
 
     fun getList() {
-        try {
-            //membuka file JSON
-            val stream = assets.open("pahlawan_nasional.json")
-            val size = stream.available()
-            val buffer = ByteArray(size)
-            stream.read(buffer)
-            stream.close()
-            val stringContent = String(buffer, StandardCharsets.UTF_8)
+        val stringContent = getJSONObject()
 
-            try {
-                val jsonObject = JSONObject(stringContent)
-                val jsonArray = jsonObject.getJSONArray("daftar_pahlawan")
-                for (i in 0 until jsonArray.length()) {
-                    val jsonObjectData = jsonArray.getJSONObject(i)
+        try {
+            val jsonArray = JSONArray(stringContent)
+
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                val jsonItemObject = jsonObject.getJSONObject("batters")
+                val jsonArrayItem = jsonItemObject.getJSONArray("batter")
+
+                for (j in 0 until jsonArrayItem.length()) {
+                    val jsonObjectData = jsonArrayItem.getJSONObject(j)
                     val dataApi = ModelPahlawan()
-                    dataApi.nama = jsonObjectData.getString("nama")
-                    dataApi.namaLengkap = jsonObjectData.getString("nama2")
-                    dataApi.image = jsonObjectData.getString("img")
+                    dataApi.nama = jsonObjectData.getString("id")
+                    dataApi.namaLengkap = jsonObjectData.getString("type")
+                    dataApi.image = jsonObjectData.getString("type")
                     modelPahlawan.add(dataApi)
                 }
-                pahlawanAdapter = PahlawanAdapter(modelPahlawan)
-                binding.rvListPahlawan.adapter = pahlawanAdapter
-
-            } catch (e: JSONException) {
-                e.printStackTrace()
             }
 
-        } catch (e: JSONException) {
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
 
+            pahlawanAdapter = PahlawanAdapter(modelPahlawan)
+            binding.rvListPahlawan.adapter = pahlawanAdapter
+
+        } catch (e: JSONException) {
+            e.printStackTrace()
         }
+
+    }
+
+    fun getJSONObject(): String {
+        return try {
+            assets.open("cake.json").bufferedReader().readText()
+//            val size = stream.available()
+//            val buffer = ByteArray(size)
+//            stream.read(buffer)
+//            stream.close()
+//
+//            String(buffer, StandardCharsets.UTF_8)
+
+        } catch (e: JSONException) {
+            Toast.makeText(this, "Opps, coba lagi", Toast.LENGTH_SHORT).show()
+            ""
+        }
+
     }
 }
